e94c599f1f1915f45ea0c4bbd4deeebb7c146952
Patrick Zurek <pzurek@uillinois.edu>
2014-04-27 00:18:15 -0500

    added rotating turret

--- core/src/net/uofitorn/scorchedearth/GameScreen.java
+++ core/src/net/uofitorn/scorchedearth/GameScreen.java
@@ -6,18 +6,20 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.Shape;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class GameScreen implements Screen {
@@ -29,68 +31,51 @@ public class GameScreen implements Screen {
 
 	int midpoints = 1;
 	
-	
 	Texture leftArrowTexture;
 	Texture rightArrowTexture;
+	Texture upArrowTexture;
+	Texture downArrowTexture;
 	Rectangle leftArrowRect;
 	Rectangle rightArrowRect;
+	Rectangle upArrowRect;
+	Rectangle downArrowRect;
 
 	OrthographicCamera camera;
 	Tank tank1;
 	float tankX;
 	float tankY;
+	int gunAngle = 0;
 	
 	boolean missileLaunched = false;
 	
 	
 	World world;
 	Box2DDebugRenderer debugRenderer;
+	float scale;
+	
+	BodyDef bodyDef;
+	Body body;
+	
+	BodyDef bulletBodyDef;
+	Body bulletBody;
+	
+	
+	Sprite tankSprite;
+	Sprite gunSprite;
 	
 	public GameScreen(final ScorchedEarthMain game) {
 		this.game = game;
-				
-		leftArrowTexture = new Texture(Gdx.files.internal("cursor_left.png"));
-		rightArrowTexture = new Texture(Gdx.files.internal("cursor_right.png"));
-		leftArrowRect = new Rectangle();
-		leftArrowRect.x = 800 - (Constants.ARROW_WIDTH * 3);
-		leftArrowRect.y = 0;
-		leftArrowRect.height = Constants.ARROW_HEIGHT;
-		leftArrowRect.width = Constants.ARROW_WIDTH;
-		rightArrowRect = new Rectangle();
-		rightArrowRect.x = 800 - Constants.ARROW_WIDTH - Constants.ARROW_WIDTH/2;
-		rightArrowRect.y = 0;
-		rightArrowRect.height = Constants.ARROW_HEIGHT;
-		rightArrowRect.width = Constants.ARROW_WIDTH;
-
+		scale = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
 		
+		initStaticTextures();
+
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		//camera = new OrthographicCamera();
 		//camera.setToOrtho(false, 800, 480);
-		camera = new OrthographicCamera(20,
-				20 * (Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth()));
-	
-		float height = 3.75f;
-		float displace = 4 / 4;
-		float roughness = 0.75f;
-		points[0] = height/2 + (MathUtils.random() * displace * 2) - displace;
-		points[Constants.POWER] = height/2 + (MathUtils.random() * displace * 2) - displace;
 		
-		displace *= roughness;
+		camera = new OrthographicCamera(20, 20 * scale);
 		
-		for (int i = 1; i < Constants.POWER; i *= 2) {
-			for (int j = (Constants.POWER/i)/2; j < Constants.POWER; j += Constants.POWER/i) {
-				points[j] = ((points[j - (Constants.POWER/i) / 2] + points[j + (Constants.POWER / i)/2]) / 2);
-				points[j] += (MathUtils.random() * displace * 2) - displace;
-			}
-			displace *= roughness;
-		}	
-		/*
-		tank1 = new Tank(points);
-		tank1.setX(100); */
-		for (int i = 0; i < Constants.POWER; i++) {
-			Gdx.app.debug("1", "1. i: " + points[i]);
-		}
-		
+		generateTerrainPoints();
 		
 		world = new World(new Vector2(0, -10), true);
 		debugRenderer = new Box2DDebugRenderer();
@@ -99,7 +84,7 @@ public class GameScreen implements Screen {
 		Vector2[] vecs = new Vector2[4];
 		BoxProp[] boxes = new BoxProp[Constants.POWER];
 		for (int i = 0; i < Constants.POWER; i++) {
-			Gdx.app.debug("GameScreen", "On i: " + i);
+			//Gdx.app.debug("GameScreen", "On i: " + i);
 			vecs[0] = new Vector2();
 			vecs[0].x = (float)i * step - 10;
 			vecs[0].y = -camera.viewportHeight/2;
@@ -115,25 +100,37 @@ public class GameScreen implements Screen {
 			boxes[i] = new BoxProp(world, vecs);
 		}
 
-		CircleShape circleShape = new CircleShape();
-		circleShape.setRadius(0.1f);
-		Shape shape = circleShape;
+		CircleShape bulletShape = new CircleShape();
+		bulletShape.setRadius(0.1f);
+		FixtureDef bulletFixtureDef = new FixtureDef();
+		bulletFixtureDef.shape = bulletShape;
+		bulletFixtureDef.isSensor = false;
+		bulletFixtureDef.density = 1.0f;
+		bulletFixtureDef.friction = 0.25f;
+		bulletFixtureDef.restitution = 0.0f;
+		bulletBodyDef = new BodyDef();
+		bulletBodyDef.type = BodyType.DynamicBody;
+		
+		
+		PolygonShape tankShape = new PolygonShape();
+		//tankShape.setAsBox(1.0f, 0.5f);
+		tankShape.setAsBox(2.0f / 2.0f, (2.0f * scale) / 2.0f);
 		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = shape;
+		fixtureDef.shape = tankShape;
 		fixtureDef.isSensor = false;
-		fixtureDef.density = 2.5f;
+		fixtureDef.density = 4.5f;
 		fixtureDef.friction = 0.25f;
-		fixtureDef.restitution = 0.75f;
-		BodyDef bodyDef = new BodyDef();
+		fixtureDef.restitution = 0.0f;
+		
+		bodyDef = new BodyDef();
 		bodyDef.type = BodyType.DynamicBody;
-		bodyDef.position.set(new Vector2(0, 0));
+		bodyDef.position.set(new Vector2(25*step - 10, points[25]));
 
-		Body body = world.createBody(bodyDef);
+		body = world.createBody(bodyDef);
 		body.createFixture(fixtureDef);
-
+		body.setUserData(tankSprite);
+		
 		fixtureDef.shape.dispose(); 
-		
-		
 	}
 	
 	@Override 
@@ -141,10 +138,11 @@ public class GameScreen implements Screen {
 		Gdx.gl.glClearColor(0.2f, 0.6f, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        
     	debugRenderer.render(world, camera.combined);
         
         // tell the camera to update its matrices.
-        //camera.update();
+        camera.update();
         
         // tell the SpriteBatch to render in the
         // coordinate system specified by the camera.
@@ -158,26 +156,52 @@ public class GameScreen implements Screen {
         float step = 20 / (float)Constants.POWER;
         for (int i = 0; i < Constants.POWER; i++) {
         	shapeRenderer.line((float)i * step - 10, points[i], ((float)i*step) + step - 10, points[i+1]);
-    		Gdx.app.debug("2", "2. i: " + points[i]);
+    		//Gdx.app.debug("2", "2. i: " + points[i]);
         }
         shapeRenderer.end();
-        /*game.batch.begin();
-        game.batch.draw(leftArrowTexture, 800-64-64-64, 0);
-        game.batch.draw(rightArrowTexture, 800-64-32, 0);
+        
+        // want to convert 64 to 
+        game.batch.begin();
+    	
+        Sprite sprite = (Sprite) body.getUserData();
+        Vector2 position = body.getPosition();
+        sprite.setPosition((position.x - sprite.getWidth() / 2.0f) + 0.05f, (position.y - sprite.getWidth() / 2.0f) + 0.05f);
+        sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+    	sprite.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
+    	
+        gunSprite.setPosition((position.x - sprite.getWidth() / 2.0f) + 0.05f, (position.y - sprite.getWidth() / 2.0f) + 0.70f);
+    	gunSprite.setRotation(gunAngle);
+    	
+    	gunSprite.draw(game.batch);
+    	sprite.draw(game.batch);
+    	
+        game.batch.draw(leftArrowTexture, leftArrowRect.x, leftArrowRect.y, leftArrowRect.width, leftArrowRect.height);
+        game.batch.draw(rightArrowTexture, rightArrowRect.x, rightArrowRect.y, rightArrowRect.width, rightArrowRect.height);
+        game.batch.draw(upArrowTexture, upArrowRect.x, upArrowRect.y, upArrowRect.width, upArrowRect.height);
+        game.batch.draw(downArrowTexture, downArrowRect.x, downArrowRect.y, downArrowRect.width, downArrowRect.height);
         game.batch.end();
-        tank1.draw(camera); */
-              
-        /*
+                    
         if (Gdx.input.isTouched()) {
+        	Gdx.app.debug("GameScreen", "Got touch event");
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
             if (leftArrowRect.contains(touchPos.x, touchPos.y)) {
-            	tank1.setX(tank1.getX() - 1);
+            	body.applyLinearImpulse(-1.3f, 0, bodyDef.position.x, bodyDef.position.y, true);
             } else if (rightArrowRect.contains(touchPos.x, touchPos.y)) {
-            	tank1.setX(tank1.getX() + 1);
-            }
-        }      */
+           		body.applyLinearImpulse(1.3f, 0, bodyDef.position.x, bodyDef.position.y, true);         	
+            } else if (downArrowRect.contains(touchPos.x, touchPos.y)) {
+            	Gdx.app.debug("GameScreen", "gunAngle: " + gunAngle);
+            	if (gunAngle <= 0) {
+            		gunAngle++;
+            	}
+        	} else if (upArrowRect.contains(touchPos.x, touchPos.y)) {
+        		Gdx.app.debug("GameScreen", "gunAngle: " + gunAngle);
+        		if (gunAngle > -180) {
+        			gunAngle--;
+        		}
+        	}
+        }      
         
     	world.step(1 / 60f, 6, 2);
 	}
@@ -207,5 +231,37 @@ public class GameScreen implements Screen {
  
     }
 
+    protected void generateTerrainPoints() {
+		float height = 3.75f;
+		float displace = 4 / 4;
+		float roughness = 0.75f;
+		points[0] = height/2 + (MathUtils.random() * displace * 2) - displace;
+		points[Constants.POWER] = height/2 + (MathUtils.random() * displace * 2) - displace;
+		displace *= roughness;		
+		for (int i = 1; i < Constants.POWER; i *= 2) {
+			for (int j = (Constants.POWER/i)/2; j < Constants.POWER; j += Constants.POWER/i) {
+				points[j] = ((points[j - (Constants.POWER/i) / 2] + points[j + (Constants.POWER / i)/2]) / 2);
+				points[j] += (MathUtils.random() * displace * 2) - displace;
+			}
+			displace *= roughness;
+		}
+    }
+    
+    protected void initStaticTextures() {
+		leftArrowTexture = new Texture(Gdx.files.internal("cursor_left.png"));
+		rightArrowTexture = new Texture(Gdx.files.internal("cursor_right.png"));
+		upArrowTexture = new Texture(Gdx.files.internal("cursor_up.png"));
+		downArrowTexture = new Texture(Gdx.files.internal("cursor_down.png"));
+		tankSprite = new Sprite(new Texture(Gdx.files.internal("tanknogun.png")));
+		tankSprite.setSize(2.0f, 2.0f * scale);
+		tankSprite.setOrigin(tankSprite.getWidth() / 2.0f, tankSprite.getHeight() / 2.0f);
+		gunSprite = new Sprite(new Texture(Gdx.files.internal("gun.png")));
+		gunSprite.setSize(1.0f, 0.5f * scale);
+		gunSprite.setOrigin(gunSprite.getWidth(), gunSprite.getHeight() / 2);
+		leftArrowRect = new Rectangle(6, -6, 1, 1);
+		rightArrowRect = new Rectangle(8, -6, 1, 1);
+		upArrowRect = new Rectangle(-8, -4, 1, 1);
+		downArrowRect = new Rectangle(-8, -6, 1, 1);
+    }
 	
 }
